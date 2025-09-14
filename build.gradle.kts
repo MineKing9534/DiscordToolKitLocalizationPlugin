fa4080c0677d2f342c50a4411037facd7ce7f056
MineKing <mineking9534@gmail.com>
2025-09-14 18:54:04 +0200

    Bump version 1.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,11 +3,12 @@ plugins {
 }
 
 val release = System.getenv("RELEASE") == "true"
-rootProject.extra["effectiveVersion"] = if (release) "${project.version}" else System.getenv("BRANCH")
 
 allprojects {
     group = "de.mineking"
-    version = "1.0.1"
+    version = "1.1.0"
+
+    rootProject.extra["effectiveVersion"] = if (release) version else System.getenv("BRANCH")
 
     apply(plugin = "maven-publish")
 
